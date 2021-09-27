e0b184240815dfff283a0897444a2afe2e661937
Desire Tillerås <desire.tilleras@gmail.com>
2021-09-27 14:52:40 +0200

    Delete

---
+++ SinusSkateboards.UI/Pages/AdminFolder/DeleteProduct.cshtml.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SinusSkateboards.Domain;
+using SinusSkateBoards.Data.Database;
+
+namespace SinusSkateboards.UI.Pages.AdminFolder
+{
+    public class DeleteProductModel : PageModel
+    {
+        private readonly AuthDbContext _context;
+        public DeleteProductModel(AuthDbContext context)
+        {
+            _context = context;
+        }
+
+        public static ProductModel Product;
+
+        public void OnGet(int id)
+        {
+          Product =  _context.Products.Where(x => x.Id == id).FirstOrDefault();
+        }
+
+        public IActionResult OnPostDelete()
+        {
+            _context.Products.Remove(Product);
+            _context.SaveChanges();
+
+            return RedirectToPage("/Index");
+        }
+        public IActionResult OnPostDontDelete()
+        {
+
+            return RedirectToPage("/AdminFolder/ListAllProducts");
+        }
+    }
+}
---
+++ SinusSkateboards.UI/Pages/AdminFolder/ListAllProducts.cshtml.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SinusSkateboards.Domain;
+using SinusSkateBoards.Data.Database;
+
+namespace SinusSkateboards.UI.Pages.AdminFolder
+{
+    public class ListAllProductsModel : PageModel
+    {
+        [BindProperty]
+        public List<ProductModel> ListOfAllProducts { get; set; }
+
+        private readonly AuthDbContext _context;
+
+        public ListAllProductsModel(AuthDbContext context)
+        {
+            _context = context;
+        }
+        public void OnGet()
+        {
+            ListOfAllProducts = _context.Products.Select(x => x).ToList();
+        }
+    }
+}
